01687f6209c8478999b1fc67318f3438deed6a3f
Gennadiy <ivge1312200@gmail.com>
2024-06-14 17:56:18 +0900

    First blood!

---
+++ js/abilities.js
@@ -0,0 +1,35 @@
+var vid = document.getElementById("abVideo");
+var ability = document.getElementById("ability")
+ability.onmouseover = function(){
+vid.play();
+}
+ability.onmouseup = function(){
+vid.stop();
+}
+
+var vid2 = document.getElementById("abVideo2");
+var ability2 = document.getElementById("ability2")
+ability2.onmouseover = function(){
+vid2.play();
+}
+ability2.onmouseup = function(){
+vid2.stop();
+}
+
+var vid3 = document.getElementById("abVideo3");
+var ability3 = document.getElementById("ability3")
+ability3.onmouseover = function(){
+vid3.play();
+}
+ability3.onmouseup = function(){
+vid3.stop();
+}
+
+var vid4 = document.getElementById("abVideo4");
+var ability4 = document.getElementById("ability4")
+ability4.onmouseover = function(){
+vid4.play();
+}
+ability4.onmouseup = function(){
+vid4.stop();
+}
---
+++ js/missions.js
@@ -0,0 +1,35 @@
+function readMore() {
+    var more = document.getElementById("more");
+    var btn = document.getElementById("btn");
+
+    if(more.style.display === "inline") {
+        more.style.display="none";
+    } else {
+        more.style.display="inline";
+    }
+
+}
+
+function readMore2() {
+    var more2 = document.getElementById("more2");
+    var btn2 = document.getElementById("btn2");
+
+    if(more2.style.display === "inline") {
+        more2.style.display="none";
+    } else {
+        more2.style.display="inline";
+    }
+
+}
+
+function readMore3() {
+    var more3 = document.getElementById("more3");
+    var btn3 = document.getElementById("btn3");
+
+    if(more3.style.display === "inline") {
+        more3.style.display="none";
+    } else {
+        more3.style.display="inline";
+    }
+
+}
